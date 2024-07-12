30038c93aa9163500a75408c58abc590bbd1363e
Abdellahz0 <abdellahzeghmar@gmail.com>
2024-07-12 14:39:30 +0100

    version 2.0

--- weather.js
+++ weather.js
@@ -2,14 +2,44 @@ const apiKey="227bf3e5f7eda40337c6476b8030533c";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchbox=document.querySelector(".search input");
 const searchBtn=document.querySelector(".searchbtn");
+const weatherIcon=document.querySelector(".weather-icon");
+
+
 async function checkweather(City){
     const response= await fetch(apiUrl+City+`&appid=${apiKey}`);
-    data= await response.json();
 
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }else{
+        document.querySelector(".error").style.display="none";
+        document.querySelector(".weather").style.display="block";
+        data= await response.json();
+    console.log(data);
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+"°C";
     document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
     document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+
+
+    switch(data.weather[0].main){
+        case "Clouds":
+        weatherIcon.src="https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-22-128.png";
+        break;
+        case"Clear":
+        weatherIcon.src="https://cdn3.iconfinder.com/data/icons/tango-icon-library/48/weather-clear-128.png";
+        break;
+        case "Rain":
+        weatherIcon.src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_16-128.png";
+        break;
+        case "Drizzle":
+        weatherIcon.src="https://cdn3.iconfinder.com/data/icons/spring-23/32/rain-rainfall-drizzle-sun-cloud-weather-forecast-128.png";
+        break;
+        case "Mist":
+        weatherIcon.src="https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_39-128.png";
+        break;
+    }
+    }
     
 }    
 
